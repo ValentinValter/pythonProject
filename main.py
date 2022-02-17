345ef6a49dc8676f108f8a1b6fb6d817ec5fc6ad
ValentilValter <v9333202134@gmail.com>
2022-02-17 18:42:38 +0700

    изменил название функции

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def print_hi(name):
+def add_numb(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    add_numb('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
