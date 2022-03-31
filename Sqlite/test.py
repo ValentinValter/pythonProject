287b551025dbd74e48ce5472b5704679d4157c8d
ValentilValter <V9333202134@gmail.com>
2022-03-31 17:46:32 +0700

    Добавил тест, в котором проверяется действительно ли есть такое имя в таблице.

---
+++ Sqlite/test.py
@@ -0,0 +1,22 @@
+import unittest
+import sqlite3 as sq
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        # Подключаемся к базе данных sqlite
+        sq_connction = sq.connect('users.db')
+        cursor = sq_connction.cursor()
+
+        # Выбор нужных полей из таблицы
+        sq_select = """SELECT name from users LIMIT 1"""
+        # Выполнить мой запрос
+        cursor.execute(sq_select)
+        # Получаем данные из sqlite превращаем их в данные python и присваиваем переменную
+        records = cursor.fetchall()
+        cursor.close()
+
+        self.assertEqual(records, [('Kirpich',)])  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
